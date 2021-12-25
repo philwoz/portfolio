db1c3313f19a4d5c715698cb7c177b83d1c796ba
philwoz <61164019+philwoz@users.noreply.github.com>
2021-12-25 13:49:41 +0000

    created components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Cv from "./components/Cv";
+
 
 export default function App() {
-  return "App";
+  return (
+    <main className="text-gray-400 bg-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Cv />
+      <Contact />
+    </main>
+  );
 }
---
+++ src/components/Cv.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Cv(){
+    return <h1>View my CV</h1>
+}
