20e710873dd2fa41174c4ebb84eb56b7ac2e2f36
philwoz <61164019+philwoz@users.noreply.github.com>
2022-01-09 20:41:21 +0000

    added project description text

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "footyForecast",
     subtitle: "React",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "This is the is the m.v.p for my final project on my bootcamp course. Built with React and uses Axios to fetch data from www.api-football.com. My aim is to continue this project and combine it with a python backend api system that delivers my own football analysis data.",
     image: "./statApp.gif",
     link: "https://philwoz.github.io/statApp/",
   },
@@ -11,7 +11,7 @@ export const projects = [
     title: "Django Market",
     subtitle: "Django and Python",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "This is a full-stack mock e-commerce website built with Python, Django and SqlLite. My aim was to develop my python skills and explore new frameworks for frontend development.",
     image: "./django.gif",
     link: "http://wwoozz.pythonanywhere.com/home/",
   },
@@ -19,7 +19,7 @@ export const projects = [
     title: "Restaurants Review",
     subtitle: "React and mongoDB",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "This full-stack Restaurant review website project built using Javascript, React, Node, Express and MongoDb. My aim was to consolidate and further develop skills I learnt at codenation.",
     image: "./merncode.gif",
     link: "https://github.com/philwoz/restaurants",
   },
@@ -27,7 +27,7 @@ export const projects = [
     title: "Flask Market",
     subtitle: "Flask and Python",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "This is another full-stack mock e-commerce website, but this is built with Python, Flask and SqlLite. Again my aim was to learn how to create web applications using Python.",
     image: "./flask.gif",
     link: "https://github.com/philwoz/FlaskMarket",
   },
